9741e22bb801e50e0b48d3956d0473f5d2e9d7c6
Michael Lee <mzlee@cs.utexas.edu>
2013-12-05 18:54:48 -0600

    A bit of cleanup

--- include/site-script.js
+++ include/site-script.js
@@ -1,6 +1,6 @@
 function selectPage(selector) {
-   html = $("#" + selector + "-data .info").html();
-   bgurl = $("#" + selector + "-data .background").text();
+   html = $("#"+selector+"-data .info").html();
+   bgurl = $("#"+selector+"-data .background").text();
    $("#content").html(html);
    $("#content").css('background', bgurl);
    $("#title").text($("#"+selector).text());
@@ -11,8 +11,8 @@ function selectPage(selector) {
 
 function setupEvents() {
    $(".nav li a").bind("click", function () {
-	 selectPage(this.id);
-      });
+      selectPage(this.id);
+   });
 }
 
 function setupAddress() {
@@ -28,18 +28,18 @@ function setupAddress() {
 }
 
 $(document).ready(function() {
-      setupEvents();
-      setupAddress();
-      var url = window.location.href,
-	 i, j, target;
-      i = url.indexOf("#!");
-      j = url.indexOf("?");
-      if (i < 0) {
-	 target = "about";
-      } else if (j < 0) {
-	 target = url.substring(i + 3);
-      } else {
-	 target = url.substring(i + 3, j);
-      }
-      selectPage(target);
-   });
+   setupEvents();
+   setupAddress();
+   var url = window.location.href,
+   i, j, target;
+   i = url.indexOf("#!");
+   j = url.indexOf("?");
+   if (i < 0) {
+      target = "about";
+   } else if (j < 0) {
+      target = url.substring(i + 3);
+   } else {
+       target = url.substring(i + 3, j);
+   }
+   selectPage(target);
+});
